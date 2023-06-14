6c95c63c214916e9d536d40354988cb75307dee9
Julio <killer.of.time@protonmail.com>
2023-06-14 17:08:26 +1000

    renamed controller

---
+++ Vendor/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vendor.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            var uid = HttpContext.Session.GetString("user");
+            if (string.IsNullOrEmpty(uid)) return Redirect("http://localhost:5231");
+            return Ok("hello there!, " + uid);
+        }
+    }
+}
